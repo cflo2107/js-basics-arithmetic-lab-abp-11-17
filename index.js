b9f9f1b26e0bceddd75e00fa30a3c3d1507a5f21
Cristina Florig <cflo2107@gmail.com>
2017-11-15 00:17:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ var newID = 1000000000 + oldID
 var ageIsValid = Number.isInteger (currentAge)
 var randomNumber = Math.random(0, 20)
 var randomInteger = Math.floor(randomNumber)
-var randomUserID = `${randomInteger}`Math.floor(1000000001, 1000000020)
+var randomUserID = `${randomInteger}` 
